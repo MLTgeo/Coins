52d8d6f30cb471055b6385991f893883093a9003
marion <marion@ubuntu>
2012-08-27 11:57:55 +0200

    Variations of the values of input parameters in continuous 2 and 3 - 4

--- src/main/scala/fr/iscpif/coin/continuous3/Simulation.scala
+++ src/main/scala/fr/iscpif/coin/continuous3/Simulation.scala
@@ -48,7 +48,7 @@ object Simulation extends App {
         case 0 => gaussian(1, 1, rng)
         case 1 => gaussian(1, 1, rng)
       }
-      override val steps = 200
+      override val steps = 1000
     }
     
   
