c6249e0f65a53d58645aa0b1c034cbf3eec5d169
Marion Le Texier <mlt@parisgeo.cnrs.fr>
2012-12-17 17:37:26 +0100

    Towns file

--- model/src/main/scala/fr/iscpif/coin/circular/Simulation.scala
+++ model/src/main/scala/fr/iscpif/coin/circular/Simulation.scala
@@ -22,6 +22,8 @@ import java.util.Random
 import scala.annotation.tailrec
 import scala.io.Source
 import fr.iscpif.coin.tool.Parse
+import fr.iscpif.coin.city.City
+import fr.iscpif.coin.circular.City
 
 object Simulation extends App {
   //  val city1 = new City(0, 0, 100, math.cos(0), math.sin(0))
@@ -32,14 +34,32 @@ object Simulation extends App {
   val city2 = new City(1, 1, 100, 1, 2)
   val city3 = new City(2, 2, 1000, 1, 3)
 
+/* // val param = Parse(args)
+
+ param.results.mkdirs
+
+  val townMatrix =
+    Source.fromFile(param.towns).getLines.drop(1).filterNot(_.matches(" *")).map {
+      l => l.split("\t").toArray
+    }
+  val cities = townMatrix.map {
+    line =>
+      val idCity = line(0).toInt
+      //val x = line(1).toDouble
+      //val y = line(2).toDouble
+      val idCountry = line(3).toInt
+      val rank = line(4).toInt
+      new City(idCity, idCountry, rank)
+  }.toIndexedSeq*/
+
   val cities = List(city1, city2, city3)
 
   val model =
     new Model {
-      def distanceDecay: Double = 10
+      def distanceDecay: Double = 2
       def populationWeight: Double = 1
       def mobilRate(city: City): Double = 0.5
-      def steps = 10
+      def steps = 100
       def endOfStep(s: Int, agents: Iterable[Agent]) =
         agents.groupBy(_.city.id).map {
           case (c, a) =>
